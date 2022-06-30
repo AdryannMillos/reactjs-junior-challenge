2e2d27ac84fd83cb203436e9166043e30a0a0ce2
Adryann Millos <adryann312@gmail.com>
2022-06-29 21:23:07 -0300

    test(userLoginService) Add success case

--- backend/src/modules/user/tests/units/userServices/userLoginService.test.js
+++ backend/src/modules/user/tests/units/userServices/userLoginService.test.js
@@ -1,30 +1,27 @@
 const { execute } = require("../../../services/userLoginService");
+const userCreate = require("../../../services/userCreateService");
 const userRepository = require("../../../repositories/userRepository");
 
 describe("Post login a user", () => {
   const previousEmail = "adrynn@123.com";
 
-//   test("should return a jwt token", async () => {
-//     user = await userRepository.create({
-//         userName: "adaryab",
-//         email: previousEmail,
-//         password: "$2b$10$1MzAliE1/L/dFzNJfdaVQupGJkPD19O.2Wkro1Mn6QjGK1.3oSDh2",
-//     });
+  test("should return a jwt token", async () => {
+    user = await userCreate.execute("adaryab", previousEmail, "123Ab!");
 
-//     await expect(execute(previousEmail, "123Ab!")).resolves.toBe('string');
-//   });
+    await expect(execute(previousEmail, "123Ab!")).resolves;
+  });
 
-    test("should return a Email, User Name or password not found", async () => {
-      await expect(
-        execute("adryab", "123Ab!")
-      ).rejects.toThrow("Email, User Name or password not found");
-      await expect(
-          execute("previousEmail@gmail.com", "123Ab!")
-        ).rejects.toThrow("Email, User Name or password not found");
-        await expect(
-          execute("previousEmail@gmail.com", "123Ab!!")
-        ).rejects.toThrow("Email, User Name or password not found");
-    });
+  test("should return a Email, User Name or password not found", async () => {
+    await expect(execute("adryab", "123Ab!")).rejects.toThrow(
+      "Email, User Name or password not found"
+    );
+    await expect(execute("previousEmail@gmail.com", "123Ab!")).rejects.toThrow(
+      "Email, User Name or password not found"
+    );
+    await expect(execute("previousEmail@gmail.com", "123Ab!!")).rejects.toThrow(
+      "Email, User Name or password not found"
+    );
+  });
 
   afterAll(async () => {
     await userRepository.destroyByFilters({ where: { email: previousEmail } });
